9e4b24ece1790a3a2bca10b389ce2f1769c5cc7e
Michal <ms@bsx.pl>
2020-03-20 19:15:43 +0800

    inital commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+## Starter for simple js project
+
+Includes:
+
++ webpack setup for css
++ postcss
++ tailwind css
+
+How to run?
+
+first: `npm install`
+
+then `npm run build` or `npm run dev`
+
+and visit /dist/index.html
+
+### Important notes:
+
+contains `CleanWebpackPlugin` which deletes content of /dist folder
+
+### Author
+
+Michal Msatbsx
+
+## License
+
+MIT
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const path = require('path');
+
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  plugins: [
+
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  }
+}
